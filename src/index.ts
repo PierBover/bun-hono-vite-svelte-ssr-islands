a0b6a8fd70aa47be5c8b669216ff69ddc31dbb45
Pier Bover <pierbover11@gmail.com>
2026-04-27 12:20:59 -0600

    basic svelte ssr

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import {Hono} from 'hono';
+import { render } from 'svelte/server';
+import Page from './Page.svelte';
 
 const app = new Hono();
 
 app.get('/', (c) => {
-	return c.body('Hello hono');
+	const result = render(Page);
+	return c.html(result.body);
 })
 
 export default app;
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import devServer from '@hono/vite-dev-server';
+import bunAdapter from '@hono/vite-dev-server/bun';
+
+export default defineConfig({
+	plugins: [
+		svelte(),
+		devServer({
+			entry: 'src/index.ts',
+			adapter: bunAdapter
+		})
+	]
+});
